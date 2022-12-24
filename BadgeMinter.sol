a6d7d8f6a0718871f88aa355fea739f553127e7d
contract <contract>
2022-12-24 22:06:41 +0800

    a

--- BadgeMinter.sol
+++ BadgeMinter.sol
@@ -4,95 +4,133 @@ pragma solidity ^0.8.2;
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/draft-EIP712Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/math/SafeMathUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
 import "hardhat/console.sol";
 
-contract BadgeMinter is
+contract BadgeMinterV2 is
 Initializable,
 AccessControlUpgradeable,
 UUPSUpgradeable,
 EIP712Upgradeable
 {
+    using SafeMathUpgradeable for uint256;
+
     address public badgeAddress;
     address public feeCollectorAddress;
     address public signer;
     uint256 public requiredClaimFee;
     bytes32 public _CLAIM_TYPEHASH;
+    bytes32 public _MIGRATE_TYPEHASH;
+
+    modifier isSufficientBalance(uint256 burnTokenCnt) {
+        require((address(this).balance >= requiredClaimFee.mul(burnTokenCnt)), "BadgeMinterV2: Insuffient balance to refund");
+
+        _;
+    }
 
     /// @custom:oz-upgrades-unsafe-allow constructor
     constructor() initializer {
     }
 
-    function initialize(
-        address _badgeAddress,
-        address _feeCollectorAddress
-    ) public initializer {
-        __AccessControl_init();
-        __UUPSUpgradeable_init();
-        __EIP712_init("BadgeMinter", "1.0.0");
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setBadgeAddress(_badgeAddress);
-        _setFeeCollectorAddress(_feeCollectorAddress);
+    function initializeV2() reinitializer(2) public {
+        __EIP712_init("BadgeMinterV2", "2.0.0");
 
-        signer = msg.sender;
-        requiredClaimFee = 0;
-        _CLAIM_TYPEHASH = keccak256(
-            "Claim(address owner,uint256 badgeId)"
+        _MIGRATE_TYPEHASH = keccak256(
+            "Migrate(address owner,uint256[] burnBadgeIds,uint256 claimBadgeId)"
         );
     }
 
+
     function setBadgeAddress(address _badgeAddress)
-    external
+    external virtual
     onlyRole(DEFAULT_ADMIN_ROLE)
     {
         _setBadgeAddress(_badgeAddress);
     }
 
     function setFeeCollectorAddress(address _feeCollectorAddress)
-    external
+    external virtual
     onlyRole(DEFAULT_ADMIN_ROLE)
     {
         _setFeeCollectorAddress(_feeCollectorAddress);
     }
 
     function setSigner(address _signer)
-    external
+    external virtual
     onlyRole(DEFAULT_ADMIN_ROLE)
     {
         signer = _signer;
     }
 
     function setClaimFee(uint256 _claimFee)
-    external
+    external virtual
     onlyRole(DEFAULT_ADMIN_ROLE)
     {
         requiredClaimFee = _claimFee;
     }
 
+    function transferEther()
+    public payable
+    onlyRole(DEFAULT_ADMIN_ROLE)
+    {
+    }
+
     function claim(
         uint256 badgeId,
         bytes memory signature
-    ) public payable {
+    ) public payable virtual {
         address _to = msg.sender;
-        _verify(_to, badgeId, signature);
+        bytes32 digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    _CLAIM_TYPEHASH,
+                    _to,
+                    badgeId
+                )
+            )
+        );
+        _verify(digest, signature);
         _claim(_to, badgeId);
     }
 
-    function getBadgeAddress() public view returns (address) {
+    function migrate(
+        uint256[] memory burnBadgeIds,
+        uint256 claimBadgeId,
+        bytes memory signature
+    ) public payable virtual isSufficientBalance(burnBadgeIds.length) {
+        address _to = msg.sender;
+        // https://github.com/ethereum/EIPs/blob/master/EIPS/eip-712.md#definition-of-encodedata
+        // Must encode array
+        bytes32 digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    _MIGRATE_TYPEHASH,
+                    _to,
+                    keccak256(abi.encodePacked(burnBadgeIds)),
+                    claimBadgeId
+                )
+            )
+        );
+        _verify(digest, signature);
+        _migrate(_to, burnBadgeIds, claimBadgeId);
+    }
+
+    function getBadgeAddress() public view virtual returns (address) {
         return badgeAddress;
     }
 
-    function getFeeCollectorAddress() public view returns (address) {
+    function getFeeCollectorAddress() public view virtual returns (address) {
         return feeCollectorAddress;
     }
 
-    function getClaimFee() public view returns (uint256) {
+    function getClaimFee() public view virtual returns (uint256) {
         return requiredClaimFee;
     }
 
-    function getSigner() public view returns (address) {
+    function getSigner() public view virtual returns (address) {
         return signer;
     }
 
@@ -118,20 +156,9 @@ EIP712Upgradeable
     }
 
     function _verify(
-        address owner,
-        uint256 badgeId,
+        bytes32 digest,
         bytes memory signature
-    ) internal view {
-        bytes32 digest = _hashTypedDataV4(
-            keccak256(
-                abi.encode(
-                    _CLAIM_TYPEHASH,
-                    owner,
-                    badgeId
-                )
-            )
-        );
-
+    ) internal virtual {
         address _signer = ECDSAUpgradeable.recover(digest, signature);
         console.log("recovered signer: %s", _signer);
         console.log("expected signer: %s", signer);
@@ -142,7 +169,7 @@ EIP712Upgradeable
     function _claim(
         address to,
         uint256 badgeId
-    ) private {
+    ) internal virtual {
         require(msg.value >= requiredClaimFee, "_claim: fee is not enough");
         require(feeCollectorAddress != address(0), "_claim: !feeCollectorAddress");
         require(badgeAddress != address(0), "_claim: !badgeAddress");
@@ -171,9 +198,54 @@ EIP712Upgradeable
         );
     }
 
+    function _migrate(
+        address to,
+        uint256[] memory burnBadgeIds,
+        uint256 claimBadgeId
+    ) internal {
+        require(burnBadgeIds.length > 0, "_migrate: burnBadgeIds is empty");
+        require(badgeAddress != address(0), "_migrate: !badgeAddress");
+
+        uint256[] memory amounts = new uint256[](burnBadgeIds.length);
+        uint256 noneMintBadgeId = 0;
+
+        for (uint256 i = 0; i < burnBadgeIds.length; i++) {
+            amounts[i] = 1;
+        }
+        AddressUpgradeable.functionCall(
+            badgeAddress,
+            abi.encodeWithSignature(
+                "burnBatch(address,uint256[],uint256[])",
+                to,
+                burnBadgeIds,
+                amounts
+            ),
+            "_migrate: burnBatch() failed"
+        );
+        if (claimBadgeId != noneMintBadgeId) {
+            AddressUpgradeable.functionCall(
+                badgeAddress,
+                abi.encodeWithSignature(
+                    "mint(address,uint256,uint256,bytes)",
+                    to,
+                    claimBadgeId,
+                    1,
+                    ""
+                ),
+                "_migrate: mint() failed"
+            );
+        }
+        uint256 amount = claimBadgeId == noneMintBadgeId ? requiredClaimFee.mul(burnBadgeIds.length) : requiredClaimFee.mul(burnBadgeIds.length - 1);
+        AddressUpgradeable.sendValue(payable(to), amount);
+    }
+
     function _authorizeUpgrade(address newImplementation)
     internal
     override
     onlyRole(DEFAULT_ADMIN_ROLE)
     {}
+
+    function getVersion() public pure virtual returns (string memory) {
+        return "2.0.0";
+    }
 }
